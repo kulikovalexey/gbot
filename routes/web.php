44fe96d94662db0e0c994da34d98ba9fb3efeb89
alexey <developer.kulikov@gmail.com>
2018-02-22 09:45:51 +0200

    change routs

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,18 @@ Route::match(['get', 'post'], 'register', function() {
    return redirect('/');
 })->name('register');
 
-//Route::middleware(['auth'])->prefix('admin')->namespace('Backend')->name('admin.')->group(function (){
-//    Route::get('/', 'DashboardController@index')->name('index');
-//
-//    Route::get('/setting', 'SettingController@index')->name('setting.index');
-//    Route::post('/setting/store', 'SettingController@store')->name('setting.store');
-//
-//    Route::post('setting/setwebhook', 'SettingController@setwebhook')->name('setting.setwebhook');
-//    Route::post('setting/getwebhookinfo', 'SettingController@getwebhookinfo')->name('setting.getwebhookinfo');
-//});
-
 
 Route::middleware(['auth'])->prefix('admin')->namespace('Backend')->name('admin.')->group(function (){
     Route::get('/', 'DashboardController@index')->name('index');
 
     Route::get('/setting', 'SettingController@index')->name('setting.index');
     Route::post('/setting/store', 'SettingController@store')->name('setting.store');
+    Route::post('setting/setwebhook', 'SettingController@setwebhook')->name('setting.setwebhook');
+    Route::post('setting/getwebhookinfo', 'SettingController@getwebhookinfo')->name('setting.getwebhookinfo');
+});
+
+Route::post(Telegram::getAccessToken(), function() {
+    Telegram::commandHandler(true);
 });
 
 Auth::routes();
