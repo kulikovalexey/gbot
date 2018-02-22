b658fa33fa5f2eafbb33776617d8886ad40ec0e8
alexey <developer.kulikov@gmail.com>
2018-02-22 16:47:35 +0200

    test command

---
+++ app/Telegram/TestCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Telegram;
+
+use Telegram\Bot\Actions;
+use Telegram\Bot\Commands\Command;
+
+
+/**
+ * Class TestCommand
+ */
+class TestCommand extends Command
+{
+    /**
+     * @var string Command Name
+     */
+    protected $name = 'test';
+
+    /**
+     * @var array Command Aliases
+     */
+    protected $aliases = ['listcommands'];
+
+    /**
+     * @var string Command Description
+     */
+    protected $description = 'Test command, Get a list of commands';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle()
+    {
+        $commands = $this->telegram->getCommands();
+
+        $text = '';
+        foreach ($commands as $name => $handler) {
+            $text .= sprintf('/%s - %s'.PHP_EOL, $name, $handler->getDescription());
+        }
+
+        $this->replyWithMessage(compact('text'));
+    }
+}
